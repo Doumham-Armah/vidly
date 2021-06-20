8c6f0c2215c03d331450c396e9c3bc01149ce6fa
mahmoudhamra <mahmoudhamra@gmail.com>
2021-06-20 22:04:27 +0300

    added pagination component

---
+++ src/components/common/Pagination.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import _ from 'lodash'
+
+const Pagination = (props) => {
+
+    const { moviesCount, pageSize, currentPage, onPageChange } = props
+
+    //this is the numbers of pages we will need
+    const pagesCount = Math.ceil(moviesCount / pageSize)
+    
+    //if theres only 1 page dont show pagination
+    if (pagesCount === 1) return null
+
+    // an array of number of pages from [1..pagesCount]
+    const pages = _.range(1, pagesCount + 1)
+
+
+    return ( 
+        <nav>
+            <ul className="Pagination">
+                {pages.map(page => 
+                    <li className="page-item" key={page}>
+                        <a className="page-link" onClick={() => onPageChange(page)}>{page}</a>
+                    </li>)}
+                
+            </ul>
+        </nav>
+     );
+}
+ 
+export default Pagination;
